0dfb0e9efb99b896e9f87433276b602e4da87f39
Kevin <kesanc_n64@outlook.com>
2023-05-18 21:03:24 -0500

    Create the navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,25 @@
-import '@/styles/globals.css'
-import Nav from '@/components/Nav'
-import Provider from '@/components/Provider'
+import '@/styles/globals.css';
+import Nav from '@/components/Nav';
+import Provider from '@/components/Provider';
 export const metadata = {
   title: 'PromptQuest',
-  description: 'Discover and share AI Prompt'
-}
+  description: 'Discover and share AI Prompt',
+};
 
-export default function RootLayout ({ children }) {
+export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body>
-        <div className='main'>
-          <div className='gradient' />
-          <main className='app'>
-            <Nav />
-            {children}
-          </main>
-        </div>
+        <Provider>
+          <div className='main'>
+            <div className='gradient' />
+            <main className='app'>
+              <Nav />
+              {children}
+            </main>
+          </div>
+        </Provider>
       </body>
     </html>
-  )
+  );
 }
--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,5 +1,6 @@
-export default function Provider () {
-  return (
-    <h1> Provider</h1>
-  )
+'use client';
+import { SessionProvider } from 'next-auth/react';
+
+export default function Provider({ children, session }) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
